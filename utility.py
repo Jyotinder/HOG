7bbd7899ffcf30eb503453e6f48d89e9ac2fd870
Jyotinder <jyotinder.pec@gmail.com>
2016-08-30 10:24:14 -0700

    Class Vlad

--- utility.py
+++ utility.py
@@ -10,6 +10,7 @@ from sklearn.metrics import confusion_matrix
 import matplotlib.pyplot as plt
 from Vlad import vladFun
 from Sift import *
+from sklearn.externals import joblib
 
 def imlist(path):
     """
@@ -17,6 +18,70 @@ def imlist(path):
     the directory path supplied as argument to the function.
     """
     return [os.path.join(path, f) for f in os.listdir(path)]
+
+
+def testSet(setX,setY,class_id):
+    des_list = []
+    for i,image_path in enumerate(setX):
+        des=sift(image_path)
+        if des !=[] and des is not None :
+            des_list.append((image_path,setY[i],des))
+        else:
+                del setX[i]
+                del setY[i]
+    des_list = sorted(des_list, key=lambda tup: tup[1])
+    x_trainingList=[]
+    for imageClass in range(0,class_id):
+        temp=[x for x in des_list if x[1]==imageClass]
+        descriptors = np.array([], dtype=np.float).reshape(0,128)
+        for x in temp:
+            descriptors=np.vstack([descriptors,x[2]])
+        print("Load Kmean for class "+ str(imageClass))
+        filename = './Kmean/'+str(imageClass)+".pkl"
+        voc=joblib.load(filename)
+        print("Vlad for training images for class"+ str(imageClass))
+        for i in xrange(len(temp)):
+            vlad= vladFun(temp[i][2],voc)
+            print(vlad.size)
+            x_trainingList.append(vlad)
+    y_train=[]
+    for i in des_list:
+        y_train.append(i[1])
+    return x_trainingList,y_train
+
+def trainTestSet(setX,setY,class_id):
+    des_list = []
+    for i,image_path in enumerate(setX):
+        des=sift(image_path)
+        if des !=[] and des is not None :
+            des_list.append((image_path,setY[i],des))
+        else:
+                del setX[i]
+                del setY[i]
+    des_list = sorted(des_list, key=lambda tup: tup[1])
+    x_trainingList=[]
+    for imageClass in range(0,class_id):
+        temp=[x for x in des_list if x[1]==imageClass]
+        descriptors = np.array([], dtype=np.float).reshape(0,128)
+        for x in temp:
+            descriptors=np.vstack([descriptors,x[2]])
+        k = 90
+        print("Kmean for class "+ str(imageClass))
+        voc, variance = kmeans(descriptors, k, 1)
+        filename = './Kmean/'+str(imageClass)+".pkl"
+        joblib.dump(voc, filename, compress=9)
+        print("Vlad for training images for class"+ str(imageClass))
+        for i in xrange(len(temp)):
+            vlad= vladFun(temp[i][2],voc)
+            print(vlad.size)
+            x_trainingList.append(vlad)
+    print "SVM Start"
+    y_train=[]
+    for i in des_list:
+        y_train.append(i[1])
+    return x_trainingList,y_train
+
+
 
 def direcrtoryProcessing(train_path):
     training_names = os.listdir(train_path)
@@ -37,54 +102,16 @@ def direcrtoryProcessing(train_path):
     X_train, X_test, y_train, y_test = train_test_split(image_paths, image_classes, test_size=0.2,random_state=0)
     ################################################
     # List where all the descriptors are stored
-    des_list = []
-    descriptors = np.array([], dtype=np.float).reshape(0,128)
-    for i,image_path in enumerate(X_train):
-        des=sift(image_path)
-        if des !=[] and des is not None :
-            des_list.append((image_path,y_train[i],des))
-        else:
-                del X_train[i]
-                del y_train[i]
-    des_list = sorted(des_list, key=lambda tup: tup[1])
-    x_trainingList=[]
-    for imageClass in range(0,class_id):
-        temp=[x for x in des_list if x[1]==imageClass]
-        descriptors = np.array([], dtype=np.float).reshape(0,128)
-        for x in temp:
-            descriptors=np.vstack([descriptors,x[2]])
-        k = 128
-        print("Kmean for class "+ str(imageClass))
-        voc, variance = kmeans(descriptors, k, 1)
-        print("Vlad for training images for class"+ str(imageClass))
-        for i in xrange(len(temp)):
-            x_trainingList.append(vladFun(temp[i][2],voc))
-    clf = LinearSVC()
-    print "SVM Start"
-    y_train=[]
-    for i in des_list:
-        y_train.append(i[1])
+    x_trainingList,y_train=trainTestSet(X_train,y_train,class_id)
     print len(x_trainingList),"  ",len(y_train)
+    clf = LinearSVC()
     clf.fit(x_trainingList, y_train)
+    filename = './Kmean/'+"clf.pkl"
+    joblib.dump(clf, filename, compress=9)
     print "SVM End"
     #####################TEST######################
     #List where all the descriptors are stored
-    x_test=[]
-    ytest=[]
-    for i,image_path in enumerate(X_test):
-        des=sift(image_path)
-        if des !=[] and des is not None :
-            vlad=shiftToVlad(des)
-            if vlad != None:
-                 x_test.append(vlad)
-                 ytest.append(y_test[i])
-            else:
-                del X_test[i]
-                del y_test[i]
-        else:
-            del X_test[i]
-            del y_test[i]
-
+    x_test,ytest=testSet(X_test,y_test,class_id)
     predictions =  clf.predict(x_test)
     print(classification_report(ytest, predictions, target_names=training_names))
     #print predictions
