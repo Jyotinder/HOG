a2b93da57b5867fba0c17708f853d217e0c3206c
Jyotinder <jyotinder.pec@gmail.com>
2016-09-09 15:34:18 -0700

    Added PCA and SDG

--- utility.py
+++ utility.py
@@ -9,6 +9,7 @@ from sklearn.cluster import MiniBatchKMeans, KMeans
 from sklearn.metrics import classification_report
 from sklearn.metrics import confusion_matrix
 import matplotlib.pyplot as plt
+from sklearn.linear_model import SGDClassifier
 from Vlad import vladFun
 from Sift import *
 from sklearn.externals import joblib
@@ -52,27 +53,32 @@ def direcrtoryProcessing(train_path):
     image_paths = []
     image_classes = []
     class_id = 0
+
     for training_name in training_names:
         dir = os.path.join(train_path, training_name)
         class_path = imlist(dir)
         image_paths+=class_path
         image_classes+=[class_id]*len(class_path)
+
         class_id+=1
 
     ################################################
     #############SPLIT DATA#########################
-    X_train, X_test, y_train, y_test = train_test_split(image_paths, image_classes, test_size=0.2,random_state=0)
+    X_train, X_test, y_train, y_test = train_test_split(image_paths, image_classes, test_size=0.4,random_state=0)
     ################################################
     # List where all the descriptors are stored
     x_trainingList,y_train=trainTestSet(X_train,y_train)
     print len(x_trainingList),"  ",len(y_train)
-    clf = LinearSVC()
-    clf.fit(x_trainingList, y_train)
+    sdg=SGDClassifier()
+    sdg.partial_fit(x_trainingList,y_train,classes=np.unique(y_train))
+    #clf = LinearSVC()
+    #clf.fit(x_trainingList, y_train)
     print "SVM End"
     #####################TEST######################
     #List where all the descriptors are stored
     x_test,ytest=trainTestSet(X_test,y_test)
-    predictions = clf.predict(x_test)
+    #predictions = clf.predict(x_test)
+    predictions = sdg.predict(x_test)
     print(classification_report(ytest, predictions, target_names=training_names))
     #print predictions
 
