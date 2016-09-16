eeaf87fb55b8479c0c74f68b633fa43b78e25d40
Jyotinder <jyotinder.pec@gmail.com>
2016-09-16 16:38:35 -0700

    Online

--- genrator.py
+++ genrator.py
@@ -29,19 +29,24 @@ def trainTestSet(setX,setY):
     y_train=[]
     for i,image_path in enumerate(setX):
         # reduce the column of the image to 128 doesn't change the number of rows
-        des=PCA_image(image_path)
-        if des !=[] and des is not None:
-            k = 128
-            k_means = KMeans(init='k-means++', n_clusters=k, n_init=10)
-            k_means.fit(des)
-            voc = k_means.cluster_centers_
-            #reduce the size of the row to the value of k
-            vlad= vladFun(des,voc)
-            x_trainingList.append(vlad)
-            y_train.append(setY[i])
-        else:
-                del setX[i]
-                del setY[i]
+        des=siftPyramid(image_path)
+        try:
+            if des !=[] and des is not None:
+                k = 128
+                k_means = KMeans(init='k-means++', n_clusters=k, n_init=10)
+                k_means.fit(des)
+                voc = k_means.cluster_centers_
+                #reduce the size of the row to the value of k
+                vlad= vladFun(des,voc)
+                x_trainingList.append(vlad)
+                y_train.append(setY[i])
+            else:
+                    del setX[i]
+                    del setY[i]
+        except:
+            print "Exception for "+ image_path
+            del setX[i]
+            del setY[i]
     return x_trainingList,y_train
 def getrows(row,X_train,Y_train):
     X=[]
@@ -78,11 +83,11 @@ def direcrtoryProcessing(train_path):
     X,Y=trainTestSet(image_paths,image_classes)
     X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2,random_state=0)
     clf = SGDClassifier()
-    batcherator = iter_minibatches(4,X_train,y_train)
+    batcherator = iter_minibatches(10,X_train,y_train)
     for X_chunk, y_chunk in batcherator:
         clf.partial_fit(X_chunk, y_chunk,classes=np.unique(Y))
         y_predicted = clf.predict(X_test)
-        print(classification_report(y_test,y_predicted ))
+        print(classification_report(y_test,y_predicted,target_names=training_names ))
 
 
 
